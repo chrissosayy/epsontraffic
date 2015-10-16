760069363e11f9e024111af332dc97c90d406a30
sosayy@gmail.com <ETHPSITTIDETH@ETHPSITTIDETH.apo.epson.net>
2015-10-16 09:39:33 +0700

    start Day2

---
+++ app/src/main/java/com/petchploy/sittidethp/epsontraffic/MainActivity.java
@@ -0,0 +1,33 @@
+package com.petchploy.sittidethp.epsontraffic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    //explicit
+    private ListView trafficListView;
+    private Button aboutmeButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Bind Widget
+bindWidget();
+    } //main method
+
+    private void bindWidget() {
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutmeButton = (Button) findViewById(R.id.button);
+
+
+    }
+} //main class ;คือคำสั่ง
+
