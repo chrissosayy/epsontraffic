94342eb07b90a409a1721c85a02031473de1df9f
chrissosayy <sosayy@gmail.com>
2015-10-16 11:15:11 +0700

    Button controller finish

--- app/src/main/java/com/petchploy/sittidethp/epsontraffic/MainActivity.java
+++ app/src/main/java/com/petchploy/sittidethp/epsontraffic/MainActivity.java
@@ -1,5 +1,8 @@
 package com.petchploy.sittidethp.epsontraffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,12 +23,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Bind Widget
-bindWidget();
+        bindWidget();
+
+        //button controller
+        buttonControler();
+
+
+
     } //main method
+
+    private void buttonControler() {
+
+        aboutmeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+// sound effect
+                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(),R.raw.mosquito);
+                buttonMediaPlayer .start();
+
+                //intent to webview
+                Intent ObjIntent = new Intent(Intent.ACTION_VIEW);
+                ObjIntent.setData(Uri.parse("https://www.youtube.com/watch?v=Vt8SHcBXFo4"));
+                        startActivity(ObjIntent);
+
+
+            } // event
+        });
+
+
+    }
 
     private void bindWidget() {
         trafficListView = (ListView) findViewById(R.id.listView);
-        aboutmeButton = (Button) findViewById(R.id.button);
+        aboutmeButton = (Button) findViewById(R.id.btnaboutMe);
 
 
     }   // bindWidget
